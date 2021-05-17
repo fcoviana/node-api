6e8f9848b014f0b7f3624042eb78e757fff77984
Francisco Viana <franciscoviana862@gmail.com>
2021-05-16 21:28:54 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -11,19 +11,19 @@ app.use(express.json());
 app.use('/api', routes);
 
 app.use((request, response, next) => {
-    response.status(status.NOT_FOUND).send();
+  response.status(status.NOT_FOUND).send();
 });
 
 app.use((error, request, response, next) => {
-    response.status(status.INTERNAL_SERVER_ERROR).json({ error });
+  response.status(status.INTERNAL_SERVER_ERROR).json({ error });
 });
 
 sequelize.sync({ force: true }).then(() => {
-    const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3000;
 
-    app.set('port', port);
+  app.set('port', port);
 
-    const server = http.createServer(app);
+  const server = http.createServer(app);
 
-    server.listen(port);
+  server.listen(port);
 });
